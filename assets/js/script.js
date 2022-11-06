35c999550e44d3fb6df7398f87c465106e6f1ca0
fatimamahate <fatimamahate@yahoo.co.uk>
2022-11-06 14:50:53 +0000

    Add structure of images of easy difficulty and structure of event listener

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+/** 
+ * Add all image pairs for the easy difficulty as objects with information on whether they match
+ */
+let ePair1={img1:'url',
+img2:'url',
+same:'Y'}
+let ePair2={img1:'url',
+img2:'url',
+same:'Y'}
+let ePair3={img1:'url',
+img2:'url',
+same:'Y'}
+let ePair4={img1:'url',
+img2:'url',
+same:'Y'}
+let ePair5={img1:'url',
+img2:'url',
+same:'Y'}
+let ePair6={img1:'url',
+img2:'url',
+same:'Y'}
+let easyOne=[ePair1,ePair2,ePair3,ePair4,ePair5,ePair6];
+
+document.addEventListener("DOMContentLoaded",function( ){
+    if (this.getAttribute("data-type")==="easy"){
+
+    } else if (this.getAttribute("data-type")==="difficult"){
+
+    } else {
+        
+    }
+    
+})
+
+
