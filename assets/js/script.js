086126c6de58592bab792f5d8949aa04e272637e
fatimamahate <fatimamahate@yahoo.co.uk>
2022-11-06 18:42:03 +0000

    Add event listner

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,43 @@
 /** 
  * Add all image pairs for the easy difficulty as objects with information on whether they match
  */
-let ePair1={img1:'url',
-img2:'url',
-same:'Y'}
-let ePair2={img1:'url',
-img2:'url',
-same:'Y'}
-let ePair3={img1:'url',
-img2:'url',
-same:'Y'}
-let ePair4={img1:'url',
-img2:'url',
-same:'Y'}
-let ePair5={img1:'url',
-img2:'url',
-same:'Y'}
-let ePair6={img1:'url',
-img2:'url',
-same:'Y'}
-let easyOne=[ePair1,ePair2,ePair3,ePair4,ePair5,ePair6];
+// let ePair1={img1:'url',
+// img2:'url',
+// same:'Y'}
+// let ePair2={img1:'url',
+// img2:'url',
+// same:'Y'}
+// let ePair3={img1:'url',
+// img2:'url',
+// same:'Y'}
+// let ePair4={img1:'url',
+// img2:'url',
+// same:'Y'}
+// let ePair5={img1:'url',
+// img2:'url',
+// same:'Y'}
+// let ePair6={img1:'url',
+// img2:'url',
+// same:'Y'}
+// let easyOne=[ePair1,ePair2,ePair3,ePair4,ePair5,ePair6];
 
+document.addEventListener("DOMContentLoaded",function(){
+    let choices = document.getElementsByTagName("button");
+    console.log(choices)
+    for (let choice of choices){
+        choice.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "easy") {
+               alert("youclickedeasy!")
+            } else {
+                alert("idunno")
+            }
+        });
+    }
+})
+
+/**
+ * The full game is run in the main game function
+ */
 function mainGame(){
 
 }
@@ -42,5 +59,5 @@ function displayDifficult(){
 }
 
 function displayImpossible(){
-    
+
 }
